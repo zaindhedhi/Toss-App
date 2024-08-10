90155cff3534fff137ccd43442f032cf4f34de93
zaindhedhi <zaindhedhi@oulook.com>
2024-08-11 01:35:51 +0500

    tossapp-completed

--- app.js
+++ app.js
@@ -2,12 +2,27 @@
 
 
 
-
+var para = document.querySelector('#result');
+var image = document.querySelector('#image');
 
 function toss(coin){
-    console.log(coin)
-    var tossResult = Math.random()*2
-    console.log(tossResult)
-
-
+    // console.log(coin)
+    var tossResult = Math.floor(Math.random()*2)
+    // console.log(tossResult)
+    var result = ''
+    if(tossResult == 0){
+        result = 'masjid'
+    }else{
+        result = 'chand'
+    }
+    if(coin == result){
+        para.innerHTML = "Congrats! You win the Toss"
+    }else{
+        para.innerHTML ='Sorry! better luck next time'
+    }
+    if(result == "masjid"){
+        image.src = 'https://en.numista.com/catalogue/photos/pakistan/424-original.jpg'
+    }else{
+        image.src = 'https://www.foreigncurrencyandcoin.com/wp-content/uploads/2018/12/products-18814.jpg'
+    }
 }
